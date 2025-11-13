6ebc83c688f8f79e993dd66ab8c8128a25d3c5cc
AaronHilis <aaronhillis.2026@gmail.com>
2025-11-13 11:13:24 +0100

    Initial commit

---
+++ react-ui/src/components/NavbarCom.jsx
@@ -0,0 +1,121 @@
+import React from "react";
+import {
+  Form,
+  Input,
+  Label,
+  FormGroup,
+  Modal,
+  Button,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from "reactstrap";
+import $ from "jquery"
+// import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import SignUp from "./SignUp.jsx";
+import SignIn from "./SignIn.jsx";
+import HostCar from "./HostCar.jsx";
+import "../style/NavbarCom.css";
+import signUp from "./SignUp.jsx";
+import Book from "./book.js"
+// import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+
+export default class Example extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    // this.signup = this.signup.bind(this);
+    // //login
+    // this.login = this.login.bind(this);
+    this.logout = this.logout.bind(this);
+
+
+    
+    this.state = {
+      isOpen: false,
+      modal: false,
+       hideNav: window.localStorage.getItem('user')?true:false,
+       //window.localStorage.getItem('user')==null,
+       storage : window.localStorage.getItem('user')
+    };
+  }
+  componentDidMount() {
+    $("#searchtxt").hide();
+    $("#searchbtn").hide();
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen,
+      modal: !this.state.modal,
+      
+    });
+  }
+
+  logout(){
+    console.log("hhhhhhhhhhhhhh");
+    this.setState({
+      storage: window.localStorage.removeItem('user'),
+    });
+
+  }
+
+  render() {
+    return (
+      <div className="mynav">
+        <Navbar color="navbar-dark bg-dark" dark expand="md" class="nav">
+          <NavbarBrand href="/" className="NavbarBrand">
+            ParkIn
+          </NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <input
+              id="searchtxt"
+              className="form-control mr-sm-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+
+            <button
+              id="searchbtn"
+              className="btn btn-outline-success my-2 my-sm-0"
+              type="submit"
+            >
+              Search
+            </button>
+
+            <Nav className="ml-auto" navbar>
+            <NavItem style={{'display':this.state.hideNav ? 'none': 'block'}}>
+                <HostCar />
+              </NavItem>
+              <NavItem  style={{'display':this.state.hideNav ? 'none': 'block'}}>
+                <SignIn handleClick = {this.handleLoginClick}/>
+              </NavItem>
+              <NavItem style={{'display': this.state.hideNav ? 'none':'block'}}>
+                <SignUp />
+              </NavItem>
+              <NavItem>
+              <NavLink style={{'display': this.state.hideNav === false ?  'none':'block'}} href={"/"} onClick={this.logout}>Log out{this.props.buttonLabel}</NavLink>
+              </NavItem>
+             <NavItem>
+               <NavLink href="/About">About</NavLink>
+             </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
---
+++ react-ui/src/components/SignIn.jsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import { Button, Modal, NavLink, ModalHeader, ModalBody, ModalFooter, FormGroup, Label, Input } from 'reactstrap';
+import $ from "jquery";
+
+class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modal: false 
+    };
+
+    this.toggle = this.toggle.bind(this);
+    this.login = this.login.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+
+
+  }
+
+  // send post recuest from client to BE to signin as a user
+  login() {
+
+    this.toggle()
+
+    const userObj ={
+      email: this.state.email,
+      password: this.state.password
+    }
+    console.log('here signin',userObj);
+        $.ajax({
+        url: "/login",
+        type: "POST",
+        data: JSON.stringify(userObj),
+        contentType: "application/json",
+        success: function(data) {
+          window.localStorage.setItem("user", data)
+          console.log("pleasssssss", data);
+        },
+        error: function(error) {
+          console.error("errorrrrrr", error);
+        }
+      });
+
+  }
+
+  toggle() {
+    this.setState({
+      modal: !this.state.modal
+    });
+  }
+
+  handleInputChange (event) {
+    const target = event.target;
+    const name = target.name;
+    const value = target.value;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <NavLink onClick={this.toggle}>Sign in{this.props.buttonLabel}</NavLink>
+        <Modal
+          isOpen={this.state.modal}
+          toggle={this.toggle}
+          className={this.props.className}
+        >
+          <ModalHeader toggle={this.toggle}>Sign in</ModalHeader>
+          <ModalBody>
+          <FormGroup>
+          <Label for="exampleEmail">Email</Label>
+          <Input type="email" name="email" id="exampleEmail" placeholder="input your Email" value={this.state.email} onChange={this.handleInputChange} />
+        </FormGroup>
+        <FormGroup>
+          <Label for="password">Password</Label>
+          <Input  type="password" name="password" id="password" placeholder="input your password" value={this.state.password} onChange={this.handleInputChange} />
+        </FormGroup>
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={this.toggle} onClick={this.login}>Sign in</Button>{' '}
+            <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+          </ModalFooter>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
